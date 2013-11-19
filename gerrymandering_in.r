3f4c20311502742524ecb163b21838f2dfbdc17d
gmmeyer <gregory.matthew.meyer@gmail.com>
2013-11-19 16:07:48 -0500

    added more, not finished yet

--- gerrymandering_in.r
+++ gerrymandering_in.r
@@ -8,6 +8,8 @@ require(MASS)
 require(ggplot2)
 require(RColorBrewer)
 
+set.seed(1)
+
 #I'm going to separate the work into various functions.
 #I am going to be writing these functions in the most legible way,
 #since this is going to be linked to by the article, not necessarily in the most
@@ -17,10 +19,40 @@ require(RColorBrewer)
 #sight reader. Therefore, I am going to move around people rather than moving
 #around the map.
 
-RegularDistribution = function() {
+#Because of the one man, one vote rule in the US, each district has to have
+#a roughly equal population. The average population per district is ~710,767
+#as per the last census. I am going to round this down to 700,000 for our
+#purposes here.
 
+ElectoralDistribution = function(numberofdistricts, 
+                                 gerrymandering = FALSE,
+                                 howunfair = 'null', #add this later
+                                 peopleperdistrict = 700000,
+                                 includeindependents = FALSE, #addthislater
+                                 partyalignment = c(.5,.5)) {
+    #I am initializing these two vectors. One of them shows the electoral
+    #distribution over the state. The other one shows the population 
+    #of a given district.
+    state <- rep(0, numberofdistricts)
+    districtpopulation <- rep(0, numberofdistricts)
+    population = numberofdistricts * peopleperdistrict
+    person = NA
+    district = NA
+    
+    #The way this works is pretty simple. Every passby of the loop will
+    #create a random person then assign him to a district. Through  the use
+    #of the prob function
+    
+    while(sum(districtpopulation) <= population & gerrymandering = FALSE) {
+        person <- sample(c(-1,1), 1)
+    }
+    
+    while(sum(districtpopulation) <= population & gerrymandering = TRUE) {
+        person <- sample(c(-1,1), 1)
+    }
+    
+    
 }
 
-GerrymanderedDistribution = function() {
 
-}
+
