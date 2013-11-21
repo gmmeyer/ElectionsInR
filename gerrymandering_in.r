dace3088c3015bd14ccf16f6bb3bae28620be0a3
Greg Meyer <gregory.matthew.meyer@gmail.com>
2013-11-21 02:44:09 -0500

    much improved

--- gerrymandering_in.r
+++ gerrymandering_in.r
@@ -10,71 +10,41 @@ require(RColorBrewer)
 
 set.seed(1)
 
-#I'm going to separate the work into various functions.
-#I am going to be writing these functions in the most legible way,
-#since this is going to be linked to by the article, not necessarily in the most
-#computationally efficient way.
-
-#Deforming a map is quite hard to do (and far harder to understand to the
-#sight reader. Therefore, I am going to move around people rather than moving
-#around the map.
-
-#Because of the one man, one vote rule in the US, each district has to have
-#a roughly equal population. The average population per district is ~710,767
-#as per the last census. I am going to round this down to 700,000 for our
-#purposes here. 
-
 ElectoralDistribution = function(numberofdistricts, 
                                  gerrymandering = FALSE,
                                  howunfair = 'null', #add this later
+                                 demdistricts = c(1:2)
+                                 repdistricts = c(3:numberofdistricts)
                                  peopleperdistrict = 70000,
                                  includeindependents = FALSE, #addthislater
                                  partyalignment = c(.5,.5)) {
-    #I am initializing these two vectors. One of them shows the electoral
-    #distribution over the state. The other one shows the population 
-    #of a given district.
+
     state <- rep(0, numberofdistricts)
-    districtpopulation <- rep(0, numberofdistricts)
     population = numberofdistricts * peopleperdistrict
-    person = NA
-    district = NA
-    districtsdown = 0
+    people = NA
     
-    if(gerrymandering == FALSE) {
-        districtprob = rep(1/numberofdistricts, numberofdistricts)
-    }
-    
-    #The way this works is pretty simple. Every passby of the loop will
-    #create a random person then assign him to a district. Through  the use
-    #of the prob function, we will ensure that the distribution of parties
-    #statewide is proper.
-    #Each Republican added to the district will be represented by a negative number.
-    #Each Democrat, a positive number.
-    
-    #I will not assure in the ungerrymandered case that the distribution of parties
-    #is equal by district.
-    #That state would be very easy to create:  state <- rep(0, numberofdistricts).
-    #Instead, this is an experiment to see how easy it would be to randomly
-    #generate districts, and how easily the districts could be shifted towards
-    #one side or another.
-    
-    while(sum(districtpopulation) <= population & gerrymandering == FALSE) {
-        person <- sample(c(-1,1), 1)
-        district <- sample(c(1:numberofdistricts),1, prob = districtprob, 
-                           Replace = TRUE)
-        
-        state[district] = state[district] + person
-        districtpopulation[district] = districtpopulation[district] + 1
-        
-        if(districtpopulation[district] == peopleperdistrict) {
-            districtprob[district] = 0
-            districtsdown = districtsdown + 1
+    if(gerrymandering == FALSE){
+   		for(district in 1:numberofdistricts){
+   			state[district] <- sample(c(-1,1), size = peopleperdistrict, replace = T)
         }
     }
     
-    while(sum(districtpopulation) <= population & gerrymandering == TRUE) {
-        person <- sample(c(-1,1), 1)
+    if(gerrymandering == TRUE){
+    	for(district in demdistricts){ #democratic districts
+    		state[district] <- sample(c(-1,1), size = peopleperdistrict, 
+    							      replace = T, prob = c(.4,.6))
+    	}
+    	for(district in repdistricts){
+    		state[district] <- sample(c(-1,1), size = peopleperdistrict,
+    								  replace = T, prob = c(.6,.4)
+    	}
     }
+    
+    if(gerrymandering == TRUE && howunfair != 'null'){
+    #enable this later
+    }
+        
+        
 
     return(state)
 }
