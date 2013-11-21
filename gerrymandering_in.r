91f5c0f13494cc65132837283a9c2bfd15fcde3b
Greg Meyer <gregory.matthew.meyer@gmail.com>
2013-11-21 17:00:44 -0500

    moved simulation runs into own file, to make it easier to load up functions and run ad hoc. added dataframe transforms for simulation data (will probably take out, depending on if I use ggplot or not). assorted minor changes.

--- gerrymandering_in.r
+++ gerrymandering_in.r
@@ -88,27 +88,3 @@ MultipleDistribution <- function(numberoftimes, numberofdistricts, ... ) {
 	return(sim.results)
 
 }
-
-
-#Now, we'll run the simulation. And, we'll put the results into some graphs.
-run.names = rep(0, 30)
-district.names = rep(0, 20)
-
-for(i in 1:30){ run.names[i] = paste0('Run ',i) }
-for(i in 1:20){ district.names[i] = paste0('District ',i) }
-
-not.gerrymandered <- MultipleDistribution(30, 20)
-gerrymandered <- MultipleDistribution(30, 20, gerrymandering = T)
-less.gerrymandered <- MultipleDistribution(30, 20, gerrymandering = T, howunfair = c(.45,.55))
-
-not.gerrymandered.df <- data.frame(not.gerrymandered, row.names = district.names)
-names(not.gerrymandered.df) <- run.names
-gerrymandered.df <- data.frame(gerrymandered, row.names = district.names)
-names(gerrymandered.df) <- run.names
-less.gerrymandered.df <- data.frame(less.gerrymandered, row.names = district.names)
-names(less.gerrymanddered.df) <- run.names
-
-
-
-
-
